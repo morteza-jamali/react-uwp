5a2853e17e61409d74f800d5365b949db2e615f4
morteza-jamali <mortezajamali4241@gmail.com>
2021-04-29 02:36:40 +0000

    chore: github split action updated

---
+++ src/lib/react-uwp.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './react-uwp.module.scss';
+
+/* eslint-disable-next-line */
+export interface ReactUwpProps {}
+
+export function ReactUwp(props: ReactUwpProps) {
+  return (
+    <div>
+      <h1>Welcome to react-uwp!</h1>
+    </div>
+  );
+}
+
+export default ReactUwp;
